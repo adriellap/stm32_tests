d712d155b101f66801efd4599050803ad43254da
Vitor <vitor.sousa.franca@gmail.com>
2024-10-08 09:50:19 -0300

    arduino platform serial + blink working

---
+++ src/main.cpp
@@ -0,0 +1,128 @@
+/*
+#include "stm32f1xx_hal.h"
+
+UART_HandleTypeDef huart1;
+
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART1_UART_Init(void);
+
+void HAL_UART_MspInit(UART_HandleTypeDef* huart) {
+GPIO_InitTypeDef GPIO_InitStruct = {0};
+if (huart->Instance == USART1) {
+    __HAL_RCC_USART1_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    
+    // PA9 -> USART1_TX, PA10 -> USART1_RX
+    GPIO_InitStruct.Pin = GPIO_PIN_9;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_10;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
+}
+
+void MX_USART1_UART_Init(void) {
+huart1.Instance = USART1;
+huart1.Init.BaudRate = 115200;
+huart1.Init.WordLength = UART_WORDLENGTH_8B;
+huart1.Init.StopBits = UART_STOPBITS_1;
+huart1.Init.Parity = UART_PARITY_NONE;
+huart1.Init.Mode = UART_MODE_TX_RX;
+huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+if (HAL_UART_Init(&huart1) != HAL_OK) {
+    // Initialization Error
+    while (1);
+}
+}
+
+void SystemClock_Config(void) {
+RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+    while (1);
+}
+
+RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+                            | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+    while (1);
+}
+}
+
+void MX_GPIO_Init(void) {
+__HAL_RCC_GPIOC_CLK_ENABLE();
+GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+// Configure PC13 (LED)
+GPIO_InitStruct.Pin = GPIO_PIN_13;
+GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+}
+
+void send_string(const char *str) {
+while (*str) {
+    HAL_UART_Transmit(&huart1, (uint8_t *)str++, 1, HAL_MAX_DELAY);
+}
+}
+
+int main(void) {
+HAL_Init();
+SystemClock_Config();
+MX_GPIO_Init();
+MX_USART1_UART_Init();
+
+// Enviar a mensagem "hello world"
+send_string("hello world\n");
+
+while (1) {
+    // Enviar "hello world" a cada 2 segundos
+    send_string("hello world\n");
+    HAL_Delay(2000);
+}
+
+return 0;
+}*/
+
+#include <Arduino.h>
+
+void setup() {
+    // Inicializar a comunicação serial a 115200 bps
+    Serial.begin(115200);
+
+    // Configurar o pino PC13 como saída
+    pinMode(PC13, OUTPUT);
+
+    // Enviar "hello world" pela porta serial
+    Serial.println("hello world");
+}
+
+void loop() {
+    // Alternar o estado do LED
+    digitalWrite(PC13, HIGH);
+    delay(500);
+    digitalWrite(PC13, LOW);
+    delay(500);
+
+    // Enviar "hello world" pela porta serial a cada 2 segundos
+    Serial.println("hello world");
+    delay(2000);
+}
